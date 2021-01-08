8ad3b4b79bc2169b75b0fc572f4a153b607809c7
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-08 14:08:18 -0800

    dynamic hyperlink images added to reference link section through script.js

---
+++ Assets/script.js
@@ -0,0 +1,16 @@
+var hyperlink = $(".hyperlink");
+var link = [
+    "https://https://www.w3schools.com/",
+    "https://developer.mozilla.org/en-US/",
+    "http://stackoverflow.com/",
+    "https://github.com/"
+]
+
+function reference() {
+        hyperlink.append("<a href=" + link[0] + ">" + "<img src='Assets/w3.png' width='200' height = '132'" + "</a>");
+        hyperlink.append("<a href=" + link[1] + ">" + "<img src='Assets/mdn.png' width='200' height = '132'" + "</a>");
+        hyperlink.append("<a href=" + link[2] + ">" + "<img src='Assets/stack.png' width='200' height = '132'" + "</a>");
+        hyperlink.append("<a href=" + link[3] + ">" + "<img src='Assets/github.png' width='200' height = '132'" + "</a>");
+}
+reference();
+
