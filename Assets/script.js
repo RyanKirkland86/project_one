bf3f87152b7bd045aabe6091451244ec9dc41916
jmerges bootcamp <jamesmerges1@gmail.com>
2021-01-08 13:15:10 -0800

    Added news section skeleton

---
+++ Assets/script.js
@@ -0,0 +1,15 @@
+function renderNews () {
+    var queryURL = "http://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?category=technology&apiKey=6f6c9616dafb4bc3bad81c87458b32c9";
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        cors: true
+      })
+        .then(function(response) {
+            $("#articleTitlte").text(response.articles[0].title);
+            $("#articlePreview").attr("src", response.articles[0].urlToImage);
+            $("#articleContent").text(response.articles[0].description);
+    });
+}
+
+renderNews();
