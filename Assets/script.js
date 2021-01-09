c1cf99d7bf7b95b1a70aee2ddf7239a715eb8c07
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-09 14:38:33 -0800

    left comments for weather section to fix later. layout for weather section done, just need styling

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,6 @@
+
+// ===================================================================================
+
 function renderNews () {
     var queryURL = "http://cors-anywhere.herokuapp.com/http://api.mediastack.com/v1/news?access_key=c0567e5c2a6bf20ad545f455d2bf2c47";
     $.ajax({
@@ -16,6 +19,8 @@ function renderNews () {
 
 renderNews();
 
+// ===================================================================================
+
 var hyperlink = $(".hyperlink");
 var link = [
     "https://www.w3schools.com/",
@@ -31,6 +36,9 @@ function reference() {
         hyperlink.append("<a href=" + link[3] + ">" + "<img src='Assets/github.png' width='200' height = '132'" + "</a>");
 }
 reference();
+
+// ===================================================================================
+ 
 
 function getJoke () {
     var queryURL = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit&type=twopart";
@@ -48,6 +56,8 @@ function getJoke () {
 getJoke();
 
 // ===================================================================================
+
+
 var userInput;
 var buttonPress = $(".userSearch");
 var localKey = 0;
@@ -62,17 +72,33 @@ buttonPress.on("click", function () {
 
 function searchWeather() {
     var apiKey = "9975a2d1ef4e7fb59bbba6eef797ea85"
-    console.log(userInput);
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + apiKey + "&units=imperial";
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&units=imperial&appid=" + apiKey
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        var cityName = $(".columnOne");
-        cityName.append("<p>" + userInput + "" + (moment().format("MM/MD/YY")))
-        cityName.append("<p>" + "Temperature: " + response.main.temp + "</p>");
+        var columnOne = $(".columnOne");
+        var columnTwo = $(".columnTwo");
+        var columnThree = $(".columnThree");
+
+        columnOne.append("<p>" + "<strong>" + (moment().format("M/DD/YY")) + "</strong>");
+        $(".columnone").attr("src", "http://openweathermap.org/img/wn/"+ response.list[0].weather[0].icon + ".png");
+        // need to add weather icons ^code not correct for icon. need to address
+        columnOne.append("<p>" + "Temperature: " + response.list[0].main.temp + "</p>");
+        columnOne.append("<p>" + "Humidity: " + response.list[0].main.humidity + "</p>");
+        // need to add weather icons
+        columnTwo.append("<p>" + "<strong>" + (moment().format("M/DD/YY")) + "</strong>")
+        columnTwo.append("<p>" + "Temperature: " + response.list[1].main.temp + "</p>");
+        columnTwo.append("<p>" + "Humidity: " + response.list[1].main.humidity + "</p>");
+        // need to add weather icons
+        columnThree.append("<p>" + "<strong>" + (moment().format("M/DD/YY")) + "</strong>")
+        columnThree.append("<p>" + "Temperature: " + response.list[2].main.temp + "</p>");
+        columnThree.append("<p>" + "Humidity: " + response.list[2].main.humidity + "</p>");
+
 
     })
 }
+
+// ===================================================================================
